46e53cf9c1b8d2cc533d3862ea4737f0eb931d14
Emilio Ahumada <emilio.ahumada.s@gmail.com>
2022-12-03 07:20:14 -0300

    feat: creada ruta POST:/api/markets, que graba una nueva tienda

---
+++ good-meal-backend/app/Http/Controllers/MarketController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Market;
+
+class MarketController extends Controller {
+
+  /**
+   * Store a new market on DB
+   *
+   * @param Illuminate\Http\Request $request
+   * @return  App\Models\Market
+   *
+   */
+  public function save(Request $request) : Market {
+    //TODO: Validar
+    $request->validate([
+      'name' => 'required|unique:markets|max:255',
+      'address' => 'required|max:255',
+      'latitude' => ['required','regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
+      'longitude' => ['required','regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/']
+    ]);
+
+
+    $market = new Market($request->all());
+
+    $market->save();
+
+    return $market;
+  }
+}
--- good-meal-backend/routes/api.php
+++ good-meal-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarketController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::controller(MarketController::class)->group( function() {
+  Route::post('/markets','save');
 });
