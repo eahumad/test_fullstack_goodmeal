14650bf4060fc1a601c58c035496892b7e461d44
Emilio Ahumada <emilio.ahumada.s@gmail.com>
2022-12-05 00:27:19 -0300

    fix(markets), eliminado importaciones no necesarias

--- good-meal-backend/app/Http/Controllers/MarketController.php
+++ good-meal-backend/app/Http/Controllers/MarketController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Market;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log;
 
 class MarketController extends Controller {
 
